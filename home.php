7883f1ecaa82f8bd62e30dfb26a8a309f05eaaa4
haykalls <125626681+haykalls@users.noreply.github.com>
2023-03-07 19:39:18 -0800

    init

---
+++ home.php
@@ -0,0 +1,28 @@
+<div class="container vh-100">
+   <h4 class="text-center mt-3">Uji kompetensi Keahlian <br> Rekayasa Perangkat Lunak (RPL)<br> Tahun 2023</h4>
+   <hr>
+   <div class="row mt-3">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header" style="background-color: grey;">INFORMASI</div>
+        <div class="card-body">
+        Aplikasi pengaduan masyarakat ini dibuat untuk memenuhi tugas uji kompetensi keahlian RPL Tahun 2023 <br><br>
+        Petunjuk Penggunaan Aplikasi :
+        </div>
+        <div class="card-footer"style="background-color: grey;" ></div>
+    </div>
+  </div>
+    <div class="col-md-4">
+    <div class="card">
+        <div class="card-header" style="background-color: grey;">KONTAK</div>
+        <div class="card-body">
+            Dikembangkan oleh <br>
+            Nama : Muhammad Haikal Afrizal Setya <br>
+            Kelas : XII RPL 1 <br>
+            No.HP : 081284168209 <br>
+        </div>
+        <div class="card-footer" style="background-color: grey;"></div>
+    </div>
+  </div>
+  </div>
+</div>
---
+++ layouts/footer.php
@@ -0,0 +1,11 @@
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+<footer class="footer mt-3 py-3" style="background-color: grey;">
+    <div class="container">
+        <p class="text-center">UKK RPL 2023 | HAIKAL | SMK 1 KARAWANG</p>
+</div>
+</footer>
+
+<script type="text/javascript" src="../assets/js/boostrap.min.js"></script>
+</body>
+</html>
